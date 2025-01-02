0070fb58c8bbb4184cdf05e822fa604545a498d2
Jivan <jivanbhosale1232@gmail.com>
2025-01-02 15:31:56 +0530

    added github in intelli idea

---
+++ Memberclass/component_class.java
@@ -0,0 +1,37 @@
+/*
+    Component of class
+        it is used to describe compenent of class which is used to define the functionality of object.
+        1) data member
+            it is used to define attribute of object.
+            in java data member has divided memory allocation.
+            type of data member
+                1) static data member
+                it will be acess by class name and allocate memory at compile time .
+                declaration --->  static datatype indentifier = value;
+                                 static int x =10;
+                static has constant memory means it's memory will be created only time in whole program.
+                important question of static data member
+                 1) we can call static variable by object or not?
+                 --> yes we can call by object but static memory will be created at compile time hence static value
+                     will be stored in compile time memory area not in runtime memory area.
+                      hence runtime memory no use then it will refer to garbase collector.
+                2)dynamic data member
+                  this type of data member will allocate memory at runtime using object.
+                  it is also called instance variable in java.
+                  it will access by object.
+                  declaration ---> datatype identifier = value;
+                                  int x = 5;
+
+        2) member function or function
+        3) static block
+        4) initializer block
+        5) constructor
+        6) property
+        7) innner class
+
+ */
+package Memberclass;
+public class component_class {
+    public static void main(String[] args) {
+    }
+}
